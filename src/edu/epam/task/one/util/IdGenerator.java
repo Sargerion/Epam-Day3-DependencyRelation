3a3a47453e9c7c425052b3c4e552e4540d8c9f19
Sargerion <56206480+Sargerion@users.noreply.github.com>
2020-11-20 00:25:26 +0300

    Update IdGenerator.java

--- src/edu/epam/task/one/util/IdGenerator.java
+++ src/edu/epam/task/one/util/IdGenerator.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class IdGenerator {
 
-    private static final Random RND = new Random();
+    private static final Random rnd = new Random();
 
     public static int generateId() {
-        return RND.nextInt(1000000001) + 1147483646;
+        return rnd.nextInt(1000000001) + 1147483646;
     }
 
 }
